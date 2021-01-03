ca4602ce817f9a369eb408ac93d5de44637b3f5c
ZiminDmitriy <dimazimin96@yandex.ru>
2021-01-04 00:18:54 +0300

    Initialize primery project structure

---
+++ app/src/bin/main.rs
@@ -0,0 +1,24 @@
+use std::io::Result;
+
+#[macro_use]
+extern crate actix_web;
+use actix_web::App;
+use actix_web::HttpServer;
+
+
+// todo delete--------------------------------------------------------
+use actix_web::Responder;
+use actix_web::HttpResponse;
+#[get("/test")]
+async fn test() -> impl Responder {
+    return HttpResponse::Ok().body("test");
+}
+// todo delete---------------------------------------------------------
+
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    return HttpServer::new(|| {
+        App::new().service(test)                          // delete service
+    }).bind("0.0.0.0:80")?.run().await;
+}
