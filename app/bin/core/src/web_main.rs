46dcd5f8103a724e833e1825693c5d8dd57971a4
ZiminDmitriy <dimazimin96@yandex.ru>
2021-03-28 00:33:19 +0300

    refactored

--- app/bin/core/src/web_main.rs
+++ app/bin/core/src/web_main.rs
@@ -9,19 +9,24 @@ use std::io::Result;
 #[actix_web::main]
 async fn main() -> Result<()> {             // TODO default_service 
     return HttpServer::new(|| {         // TODO переместить Scopes в разные методы (https://actix.rs/docs/application/   "Configure")
-        App::new().service(
+        App::new().service(             // TODO https://actix.rs/actix-web/actix_web/struct.Scope.html (Service - что это и зачем)
             web::scope("")              // TODO guards для роутов о наличии джвт 
             // .wrap(core::actix_web_component::middleware::scope::test::CheckLogin)
-            // .route("/user/register", web::post().to(core::actix_web_component::request_handler::api::version1::mobile::_in_context_for::entity::entity::application_user::application_user::_new_for_context::authorization::Authorization::register))
-            .route("/user/cnfe", web::get().to(core::actix_web_component::request_handler::api::version1::mobile::_in_context_for::entity::entity::application_user::application_user::_new_for_context::authorization::Authorization::check_nickname_for_existing))
+            .route("/user/pre_register", web::get().to(core::actix_web_component::request_handler::api::version1::mobile::_in_context_for::entity::entity::application_user::application_user::_new_for_context::authorization::Authorization::pre_register))
+            .route("/user/register", web::post().to(core::actix_web_component::request_handler::api::version1::mobile::_in_context_for::entity::entity::application_user::application_user::_new_for_context::authorization::Authorization::register))
+            .route("/user/resend_email_for_register", web::get().to(core::actix_web_component::request_handler::api::version1::mobile::_in_context_for::entity::entity::application_user::application_user::_new_for_context::authorization::Authorization::resend_email_for_register))
             .route("/user/pre_log_in", web::post().to(core::actix_web_component::request_handler::api::version1::mobile::_in_context_for::entity::entity::application_user::application_user::_new_for_context::authorization::Authorization::pre_log_in))
-            .route("/user/cefe", web::get().to(core::actix_web_component::request_handler::api::version1::mobile::_in_context_for::entity::entity::application_user::application_user::_new_for_context::authorization::Authorization::check_email_for_existing))
+            .route("/user/resend_email_for_log_in", web::get().to(core::actix_web_component::request_handler::api::version1::mobile::_in_context_for::entity::entity::application_user::application_user::_new_for_context::authorization::Authorization::resend_email_for_log_in))
+            .route("/user/log_in", web::get().to(core::actix_web_component::request_handler::api::version1::mobile::_in_context_for::entity::entity::application_user::application_user::_new_for_context::authorization::Authorization::log_in))
+            .route("/user/check_nickname_for_existing", web::get().to(core::actix_web_component::request_handler::api::version1::mobile::_in_context_for::entity::entity::application_user::application_user::_new_for_context::authorization::Authorization::check_nickname_for_existing))
+            .route("/user/check_email_for_existing", web::get().to(core::actix_web_component::request_handler::api::version1::mobile::_in_context_for::entity::entity::application_user::application_user::_new_for_context::authorization::Authorization::check_email_for_existing))
         )
     }).bind("0.0.0.0:80")?.run().await;
 }
 
 // TODO Do not remove this block until the problems have been fixed {
-    // TODO -2 Пройтись по всем Unwrap() ( и второму аналогу)
+    // TODO -3 Пройтись по всем Unwrap() ( и второму аналогу)
+    // TODO -2 Спрятать Апи (Сделать непонятным)
     // TODO -1. Attack types (DDOS, for example)
     // TODO 0. Header Connection: Keep-alive - disable https://developer.mozilla.org/ru/docs/Web/HTTP/%D0%97%D0%B0%D0%B3%D0%BE%D0%BB%D0%BE%D0%B2%D0%BA%D0%B8/Connection
     // TODO 1. Work with JRWT via Redis, create BlacList for Access Token in Redis
