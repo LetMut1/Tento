502002f7e2ed3d5704dd91b6b172fae2eb180c29
ZiminDmitriy <dimazimin96@yandex.ru>
2021-04-26 00:26:20 +0300

    deleted migrations from postgresql

--- application/library/core/migrations/2021-01-07-000001_common/up.sql
+++ application/library/core/migrations/2021-01-07-000001_common/up.sql
@@ -7,6 +7,7 @@ CREATE TABLE pre_confirmed_application_user (       -- // TODO изучить п
     email VARCHAR NOT NULL,
     PRIMARY KEY (id)
 );
+
 CREATE TABLE application_user ( 
     id UUID NOT NULL,
     -- // TODO email - уникальное
@@ -24,59 +25,3 @@ ALTER TABLE application_user ADD CONSTRAINT email_1 UNIQUE (email);
 -- // TODO Сделать индексы на внешние ключи ( посмотреть, где это нужно)
 -- // TODO все, где есть дата експирации, удалять по крону бинарнику (или из-под бд сразу)
 -- // TODO проврить down
-
-
-
--- // TODO delete  (!!!!!!!! ПРОВЕРИТЬ down.sql)
-CREATE TABLE json_refresh_web_token (   -- // TODO Redis
-    json_access_web_token_id UUID NOT NULL,
-    application_user_log_in_token_device_id UUID NOT NULL,
-    -- // TODO  applicationuserid + device_id +уникальное
-    application_user_id UUID NOT NULL,
-    expired_at TIMESTAMPTZ NOT NULL,
-    PRIMARY KEY (json_access_web_token_id),
-    FOREIGN KEY (application_user_id) REFERENCES application_user (id)
-);
-
-CREATE TABLE json_access_web_token_black_list (        -- // TODO Redis
-    json_access_web_token_id UUID NOT NULL,
-    PRIMARY KEY (json_access_web_token_id),
-    FOREIGN KEY (json_access_web_token_id) REFERENCES json_refresh_web_token (json_access_web_token_id)
-);
-
-CREATE TABLE application_user_registration_confirmation_token (
-    id UUID NOT NULL,
-    pre_confirmed_application_user_id UUID NOT NULL,  
-    -- // TODO application_user_id  - уникальность   (два отдельно)
-    -- // TODO value - уникальное
-    -- // TODO удалять висящие кортежи (написать функцию либо через крон по бинарнику)
-    application_user_email VARCHAR NOT NULL,
-    value VARCHAR NOT NULL,
-    expired_at TIMESTAMPTZ NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (pre_confirmed_application_user_id) REFERENCES pre_confirmed_application_user (id)
-);
-
-CREATE TABLE application_user_log_in_token (    
-    id UUID NOT NULL,
-    application_user_id UUID NOT NULL,  
-    -- // TODO applicationuserid + device_id- уникальное   ((как блокировать пользователя, если он делает перебор value?))
-    -- // TODO удалять висящие кортежи (написать функцию либо через крон по бинарнику)
-    device_id UUID NOT NULL,
-    application_user_email VARCHAR NOT NULL,
-    value VARCHAR NOT NULL,
-    expired_at TIMESTAMPTZ NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (application_user_id) REFERENCES application_user (id)
-);
-
-CREATE TABLE application_user_reset_password_token (       -- // TODO Redis
-    id UUID NOT NULL,
-    application_user_id UUID NOT NULL,
-    application_user_email VARCHAR NOT NULL,
-    value VARCHAR NOT NULL,
-    expired_at TIMESTAMPTZ NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (application_user_id) REFERENCES application_user (id)
-);
--- // TODO delete 
--- application/library/core/source/diesel_component/schema/mod.rs
+++ application/library/core/source/diesel_component/schema/mod.rs
@@ -14,81 +14,14 @@ pub mod public {
     table! {
         use diesel::sql_types::*;
 
-        application_user_log_in_token (id) {
-            id -> Uuid,
-            application_user_id -> Uuid,
-            device_id -> Uuid,
-            application_user_email -> Varchar,
-            value -> Varchar,
-            expired_at -> Timestamptz,
-        }
-    }
-
-    table! {
-        use diesel::sql_types::*;
-
-        application_user_registration_confirmation_token (id) {
-            id -> Uuid,
-            pre_confirmed_application_user_id -> Uuid,
-            application_user_email -> Varchar,
-            value -> Varchar,
-            expired_at -> Timestamptz,
-        }
-    }
-
-    table! {
-        use diesel::sql_types::*;
-
-        application_user_reset_password_token (id) {
-            id -> Uuid,
-            application_user_id -> Uuid,
-            application_user_email -> Varchar,
-            value -> Varchar,
-            expired_at -> Timestamptz,
-        }
-    }
-
-    table! {
-        use diesel::sql_types::*;
-
-        json_access_web_token_black_list (json_access_web_token_id) {
-            json_access_web_token_id -> Uuid,
-        }
-    }
-
-    table! {
-        use diesel::sql_types::*;
-
-        json_refresh_web_token (json_access_web_token_id) {
-            json_access_web_token_id -> Uuid,
-            application_user_log_in_token_device_id -> Uuid,
-            application_user_id -> Uuid,
-            expired_at -> Timestamptz,
-        }
-    }
-
-    table! {
-        use diesel::sql_types::*;
-
         pre_confirmed_application_user (id) {
             id -> Uuid,
             email -> Varchar,
         }
     }
 
-    joinable!(application_user_log_in_token -> application_user (application_user_id));
-    joinable!(application_user_registration_confirmation_token -> pre_confirmed_application_user (pre_confirmed_application_user_id));
-    joinable!(application_user_reset_password_token -> application_user (application_user_id));
-    joinable!(json_access_web_token_black_list -> json_refresh_web_token (json_access_web_token_id));
-    joinable!(json_refresh_web_token -> application_user (application_user_id));
-
     allow_tables_to_appear_in_same_query!(
         application_user,
-        application_user_log_in_token,
-        application_user_registration_confirmation_token,
-        application_user_reset_password_token,
-        json_access_web_token_black_list,
-        json_refresh_web_token,
         pre_confirmed_application_user,
     );
 }
