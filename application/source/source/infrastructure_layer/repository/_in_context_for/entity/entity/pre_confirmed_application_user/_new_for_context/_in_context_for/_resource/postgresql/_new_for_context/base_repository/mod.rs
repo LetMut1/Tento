fe221c8851b707bfe26fffd33ae1ba5e7d07e3d5
ZiminDmitriy <dimazimin96@yandex.ru>
2021-07-25 18:15:03 +0300

    refactor namespace

--- application/source/source/infrastructure_layer/repository/_in_context_for/entity/entity/pre_confirmed_application_user/_new_for_context/_in_context_for/_resource/postgresql/_new_for_context/base_repository/mod.rs
+++ application/source/source/infrastructure_layer/repository/_in_context_for/entity/entity/pre_confirmed_application_user/_new_for_context/_in_context_for/_resource/postgresql/_new_for_context/base_repository/mod.rs
@@ -1,9 +1,9 @@
 use crate::infrastructure_layer::service::diesel_component::schema_descriptor::public::pre_confirmed_application_user as pre_confirmed_application_user_schema;
-use crate::infrastructure_layer::data_transfer_object::_in_context_for::infrastructure_layer::repository::_in_context_for::domain_layer::entity::entity::pre_confirmed_application_user::_new_for_context::_in_context_for::_resource::postgresql::_new_for_context::base_repository::_new_for_context::select::Select;
-use crate::infrastructure_layer::data_transfer_object::_in_context_for::infrastructure_layer::repository::_in_context_for::domain_layer::entity::entity::pre_confirmed_application_user::_new_for_context::_in_context_for::_resource::postgresql::_new_for_context::base_repository::_new_for_context::insert::Insert;
 use crate::domain_layer::entity::entity::application_user::_core::email::Email;
 use crate::domain_layer::entity::entity::pre_confirmed_application_user::pre_confirmed_application_user::PreConfirmedApplicationUser;
 use crate::domain_layer::error::base_error::base_error::BaseError;
+use crate::infrastructure_layer::data_transfer_object::_in_context_for::infrastructure_layer::repository::_in_context_for::domain_layer::entity::entity::pre_confirmed_application_user::_new_for_context::_in_context_for::_resource::postgresql::_new_for_context::base_repository::_new_for_context::insert::Insert;
+use crate::infrastructure_layer::data_transfer_object::_in_context_for::infrastructure_layer::repository::_in_context_for::domain_layer::entity::entity::pre_confirmed_application_user::_new_for_context::_in_context_for::_resource::postgresql::_new_for_context::base_repository::_new_for_context::select::Select;
 use diesel::dsl;
 use diesel::ExpressionMethods;
 use diesel::OptionalExtension;
