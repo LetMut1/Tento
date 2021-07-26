7944269e159e46d29146a0b5d17ebe9c95685c5b
ZiminDmitriy <dimazimin96@yandex.ru>
2021-07-27 02:28:51 +0300

    refactored

--- application/source/source/domain_layer/service/_in_context_for/domain_layer/entity/entity/application_user/_new_for_context/password_hash_resolver/mod.rs
+++ application/source/source/domain_layer/service/_in_context_for/domain_layer/entity/entity/application_user/_new_for_context/password_hash_resolver/mod.rs
@@ -1,7 +1,7 @@
 use crate::domain_layer::entity::entity::application_user::_core::password_hash::PasswordHash;
 use crate::domain_layer::entity::entity::application_user::_core::password::Password;
-use crate::infrastructure_layer::error::base_error::base_error::BaseError;
 use crate::domain_layer::service::_in_context_for::domain_layer::entity::entity::application_user::_core::password::_new_for_context::encoder_trait::EncoderTrait;
+use crate::infrastructure_layer::error::base_error::base_error::BaseError;
 use crate::infrastructure_layer::service::_in_context_for::domain_layer::entity::entity::application_user::_core::password::_new_for_context::encoder::Encoder;
 
 pub struct PasswordHashResolver;
