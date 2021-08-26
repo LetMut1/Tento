f93316fc37a36d7af44d290dde97fc48bcec038b
ZiminDmitriy <dimazimin96@yandex.ru>
2021-08-27 00:44:15 +0300

    added _component to PL_DTO

--- application/source/source/presentation_layer/data_transfer_object/response/_in_context_for/presentation_layer/service/actix_web_component/request_handler/api/version1/mobile/_in_context_for/domain_layer/entity/entity/channel/_new_for_context/common/_new_for_context/get_all_by/response/mod.rs
+++ application/source/source/presentation_layer/data_transfer_object/response/_in_context_for/presentation_layer/service/actix_web_component/request_handler/api/version1/mobile/_in_context_for/domain_layer/entity/entity/channel/_new_for_context/common/_new_for_context/get_all_by/response/mod.rs
@@ -1,21 +1,2 @@
-use serde::Serialize;
-
-#[derive(Serialize)]
-pub struct Response<S>
-where
-    S: Serialize 
-{
-    #[serde(rename = "r")]
-    result: Vec<S>
-}
-
-impl<S> Response<S>
-where
-    S: Serialize
-{
-    pub fn new(result: Vec<S>) -> Self {
-        return Self {
-            result
-        };
-    }
-}
+pub mod _component;
+pub mod response;
