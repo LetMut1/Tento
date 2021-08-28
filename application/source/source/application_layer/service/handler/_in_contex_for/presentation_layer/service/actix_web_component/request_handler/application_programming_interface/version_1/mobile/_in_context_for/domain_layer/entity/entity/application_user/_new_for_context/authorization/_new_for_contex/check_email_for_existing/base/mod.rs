f7bf9cf893ae746adc43f9ca01826a1d439d86c9
ZiminDmitriy <dimazimin96@yandex.ru>
2021-08-29 01:16:55 +0300

    changed

--- application/source/source/application_layer/service/handler/_in_contex_for/presentation_layer/service/actix_web_component/request_handler/application_programming_interface/version_1/mobile/_in_context_for/domain_layer/entity/entity/application_user/_new_for_context/authorization/_new_for_contex/check_email_for_existing/base/mod.rs
+++ application/source/source/application_layer/service/handler/_in_contex_for/presentation_layer/service/actix_web_component/request_handler/application_programming_interface/version_1/mobile/_in_context_for/domain_layer/entity/entity/application_user/_new_for_context/authorization/_new_for_contex/check_email_for_existing/base/mod.rs
@@ -6,24 +6,22 @@ use crate::infrastructure_layer::repository::data_provider::_in_context_for::dom
 use crate::infrastructure_layer::repository::data_provider::_in_context_for::domain_layer::entity::entity::application_user::_new_for_context::_in_context_for::_resource::postgresql::_new_for_context::base::Base as DataProviderApplicationUserPostgresql;
 use crate::infrastructure_layer::service::_in_context_for::_resource::_new_for_context::aggregate_connection_pool::AggregateConnectionPool;
 use crate::infrastructure_layer::service::_in_context_for::_resource::_new_for_context::connection_extractor::ConnectionExtractor;
-use crate::presentation_layer::data_transfer_object::request::_in_context_for::presentation_layer::service::actix_web_component::request_handler::application_programming_interface::version_1::mobile::_in_context_for::domain_layer::entity::entity::application_user::_new_for_context::authorization::_new_for_context::check_email_for_existing::base::Base as RequestBase;
-use crate::presentation_layer::data_transfer_object::response::_in_context_for::presentation_layer::service::actix_web_component::request_handler::application_programming_interface::version_1::mobile::_in_context_for::domain_layer::entity::entity::application_user::_new_for_context::authorization::_new_for_context::check_email_for_existing::base::Base as ResponseBase;
+use crate::presentation_layer::data_transfer_object::request::_in_context_for::presentation_layer::service::actix_web_component::request_handler::application_programming_interface::version_1::mobile::_in_context_for::domain_layer::entity::entity::application_user::_new_for_context::authorization::_new_for_context::check_email_for_existing::base::Base as Request;
+use crate::presentation_layer::data_transfer_object::response::_in_context_for::presentation_layer::service::actix_web_component::request_handler::application_programming_interface::version_1::mobile::_in_context_for::domain_layer::entity::entity::application_user::_new_for_context::authorization::_new_for_context::check_email_for_existing::base::Base as Response;
 use diesel::PgConnection as Connection;
 use std::sync::Arc;
 
 pub struct Base;
 
 impl Base {
-    pub fn handle(aggregate_connection_pool: Arc<AggregateConnectionPool>, base_request: RequestBase) -> Result<ResponseBase, BaseError> {
+    pub fn handle(aggregate_connection_pool: Arc<AggregateConnectionPool>, request: Request) -> Result<Response, BaseError> {
         let connection: &'_ Connection = &*ConnectionExtractor::get_postgresqlxxxdelete_connection(&aggregate_connection_pool)?;
 
-        let application_user_email: Email = Email::new(base_request.get_application_user_email());
+        let application_user_email: Email = Email::new(request.get_application_user_email());
 
-        return Ok(
-            ResponseBase::new(
-                DataProviderApplicationUserPostgresql::is_exist_by_email(connection, &application_user_email)?
-                || DataProviderApplicationUserPreConfirmedPostgesql::is_exist_by_application_user_email(connection, &application_user_email)?
-            )
-        );
+        let resut: bool = DataProviderApplicationUserPostgresql::is_exist_by_email(connection, &application_user_email)?
+        || DataProviderApplicationUserPreConfirmedPostgesql::is_exist_by_application_user_email(connection, &application_user_email)?;
+
+        return Ok(Response::new(resut));
     }
 }
