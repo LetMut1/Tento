4aae2621bc20f1783240a44866788ea34c44c086
ZiminDmitriy <dimazimin96@yandex.ru>
2022-01-09 02:25:19 +0300

    MessagePack part 2

--- project/application/source/presentation_layer/service/actix_web/middleware/mod.rs
+++ project/application/source/presentation_layer/service/actix_web/middleware/mod.rs
@@ -1,7 +1,2 @@
 pub mod authentication_resolver_factory;
 pub mod authentication_resolver;
-
-#[cfg(feature="facilitate_non_automatic_functional_testing")]
-pub mod authentication_resolver_factory_;
-#[cfg(feature="facilitate_non_automatic_functional_testing")]
-pub mod authentication_resolver_;
