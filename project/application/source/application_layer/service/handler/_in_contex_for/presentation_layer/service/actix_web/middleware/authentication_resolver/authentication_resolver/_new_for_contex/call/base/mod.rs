d19df3b643a59a23f0f620f148a8055fbcf0ebda
ZiminDmitriy <dimazimin96@yandex.ru>
2022-01-22 23:23:39 +0300

    refactored

--- project/application/source/application_layer/service/handler/_in_contex_for/presentation_layer/service/actix_web/middleware/authentication_resolver/authentication_resolver/_new_for_contex/call/base/mod.rs
+++ project/application/source/application_layer/service/handler/_in_contex_for/presentation_layer/service/actix_web/middleware/authentication_resolver/authentication_resolver/_new_for_contex/call/base/mod.rs
@@ -1,49 +1,42 @@
-use actix_web::dev::ServiceRequest;
-use actix_web::HttpMessage;
-use actix_web::web::Data;
+use actix_web::http::HeaderMap;
 use crate::domain_layer::entity::json_access_web_token::json_access_web_token::JsonAccessWebToken;
 use crate::domain_layer::error::entity_error::_component::_in_context_for::domain_layer::entity::json_access_web_token::_new_for_context::json_access_web_token_error::JsonAccessWebTokenError;
 use crate::domain_layer::error::entity_error::entity_error::EntityError;
 use crate::domain_layer::repository::data_provider::_in_context_for::domain_layer::entity::json_access_web_token_black_list::_new_for_context::_in_context_for::_resource::redis::_new_for_context::base_trait::BaseTrait as JsonAccessWebTokenBlackListDataProviderRedisTrait;
 use crate::domain_layer::service::_in_context_for::domain_layer::entity::json_access_web_token::_new_for_context::expiration_time_resolver_trait::ExpirationTimeResolverTrait;
 use crate::domain_layer::service::_in_context_for::domain_layer::entity::json_access_web_token::_new_for_context::serialization_form_resolver_trait::SerializationFormResolverTrait;
-use crate::infrastructure_layer::error::base_error::_component::logic_error::LogicError;
 use crate::infrastructure_layer::error::base_error::base_error::BaseError;
 use crate::infrastructure_layer::repository::data_provider::_in_context_for::domain_layer::entity::json_access_web_token_black_list::_new_for_context::_in_context_for::_resource::redis::_new_for_context::base::Base as JsonAccessWebTokenBlackListDataProviderRedis;
 use crate::infrastructure_layer::service::_in_context_for::domain_layer::entity::json_access_web_token::_new_for_context::expiration_time_resolver::ExpirationTimeResolver;
 use crate::infrastructure_layer::service::_in_context_for::domain_layer::entity::json_access_web_token::_new_for_context::serialization_form_resolver::SerializationFormResolver;
 use crate::infrastructure_layer::service::_in_context_for::infrastructure_layer::repository::_new_for_context::aggregate_connection_pool::AggregateConnectionPool;
 use crate::infrastructure_layer::service::_in_context_for::infrastructure_layer::repository::_new_for_context::connection_extractor::ConnectionExtractor;
+use std::sync::Arc;
 
 pub struct Base;
 
 impl Base {
     pub fn handle<'a>(
-        service_request: &'a ServiceRequest
-    ) -> Result<(), BaseError> {
-        if let Some(aggregate_connection_pool) = service_request.app_data::<Data<AggregateConnectionPool>>() {
-            if let Some(x_jawt_header_value) = service_request.headers().get("X-Jawt") {
-                if let Ok(x_jawt) = x_jawt_header_value.to_str() {
-                    let json_access_web_token: JsonAccessWebToken<'_> = SerializationFormResolver::deserialize(x_jawt)?;
-                    if !ExpirationTimeResolver::is_expired(&json_access_web_token)? {
-                        if !JsonAccessWebTokenBlackListDataProviderRedis::is_exist_by_json_access_token_id(
-                            &mut *ConnectionExtractor::get_redis_connection(&aggregate_connection_pool.clone().into_inner())?, json_access_web_token.get_id()
-                        )? {
-                            service_request.extensions_mut().insert(json_access_web_token);     // TODO В такой реализации При инъекции в РекуестХэндлер этот объкт будет склонирован. А нужно передать его самого. Актикс сейчас не позволякет это сделать. Проверить позже
-
-                            return Ok(());
-                        }
-
-                        return Err(BaseError::EntityError {entity_error: EntityError::JsonAccessWebTokenError {json_access_web_token_error: JsonAccessWebTokenError::InJsonAccessWebTokenBlackList}});
+        aggregate_connection_pool: Arc<AggregateConnectionPool>,
+        header_map: &'a HeaderMap
+    ) -> Result<JsonAccessWebToken<'static>, BaseError> {
+        if let Some(x_jawt_header_value) = header_map.get("X-Jawt") {
+            if let Ok(x_jawt) = x_jawt_header_value.to_str() {
+                let json_access_web_token: JsonAccessWebToken<'static> = SerializationFormResolver::deserialize(x_jawt)?;
+                if !ExpirationTimeResolver::is_expired(&json_access_web_token)? {
+                    if !JsonAccessWebTokenBlackListDataProviderRedis::is_exist_by_json_access_token_id(
+                        &mut *ConnectionExtractor::get_redis_connection(&aggregate_connection_pool)?, json_access_web_token.get_id()
+                    )? {
+                        return Ok(json_access_web_token);
                     }
 
-                    return Err(BaseError::EntityError {entity_error: EntityError::JsonAccessWebTokenError {json_access_web_token_error: JsonAccessWebTokenError::AlreadyExpired}});
+                    return Err(BaseError::EntityError {entity_error: EntityError::JsonAccessWebTokenError {json_access_web_token_error: JsonAccessWebTokenError::InJsonAccessWebTokenBlackList}});
                 }
+
+                return Err(BaseError::EntityError {entity_error: EntityError::JsonAccessWebTokenError {json_access_web_token_error: JsonAccessWebTokenError::AlreadyExpired}});
             }
-    
-            return Err(BaseError::EntityError {entity_error: EntityError::JsonAccessWebTokenError {json_access_web_token_error: JsonAccessWebTokenError::NotFound}});
         }
 
-        return Err(BaseError::LogicError {logic_error: LogicError::new(true, "Aggregate_connection_pool must exist in application state.")});
+        return Err(BaseError::EntityError {entity_error: EntityError::JsonAccessWebTokenError {json_access_web_token_error: JsonAccessWebTokenError::NotFound}});
     }
 }
--- project/application/source/presentation_layer/service/actix_web/middleware/authentication_resolver/mod.rs
+++ project/application/source/presentation_layer/service/actix_web/middleware/authentication_resolver/mod.rs
@@ -2,11 +2,15 @@ use actix_service::Service;
 use actix_web::dev::ServiceRequest;
 use actix_web::dev::ServiceResponse;
 use actix_web::Error;
+use actix_web::HttpMessage;
+use actix_web::web::Data;
 use crate::application_layer::service::handler::_in_contex_for::presentation_layer::service::actix_web::middleware::authentication_resolver::authentication_resolver::_new_for_contex::call::base::Base as HandlerBase;
 use crate::domain_layer::error::entity_error::_component::_in_context_for::domain_layer::entity::json_access_web_token::_new_for_context::json_access_web_token_error::JsonAccessWebTokenError;
 use crate::domain_layer::error::entity_error::entity_error::EntityError;
 use crate::domain_layer::service::_in_context_for::domain_layer::error::_new_for_context::communication_code_storage::CommunicationCodeStorage;
+use crate::infrastructure_layer::error::base_error::_component::logic_error::LogicError;
 use crate::infrastructure_layer::error::base_error::base_error::BaseError;
+use crate::infrastructure_layer::service::_in_context_for::infrastructure_layer::repository::_new_for_context::aggregate_connection_pool::AggregateConnectionPool;
 use crate::presentation_layer::service::_in_context_for::presentation_layer::service::actix_web::_new_for_context::response_creator::ResponseCreator;
 use crate::presentation_layer::service::_in_context_for::presentation_layer::service::actix_web::_new_for_context::response_data_wrapper::ResponseDataWrapper;
 use futures::future;
@@ -58,52 +62,63 @@ where
         &'a mut self,
         service_request: ServiceRequest
     ) -> Self::Future {
-        if let Err(ref base_error) = HandlerBase::handle(&service_request) {
-            match base_error {
-                BaseError::EntityError {entity_error} => {
-                    match entity_error {
-                        EntityError::JsonAccessWebTokenError {json_access_web_token_error} => {
-                            match json_access_web_token_error {
-                                JsonAccessWebTokenError::AlreadyExpired => {
-                                    match rmp_serde::to_vec(&ResponseDataWrapper::wrap_for_fail(
-                                        CommunicationCodeStorage::ENTITY_JSON_ACCESS_WEB_TOKEN_ALREADY_EXPIRED
-                                    )) {
-                                        Ok(data) => {
-                                            return Either::Right(future::ok(service_request.into_response(ResponseCreator::create_ok(data).into_body())));
+        if let Some(data) = service_request.app_data::<Data<AggregateConnectionPool>>() {
+            match HandlerBase::handle(data.clone().into_inner(), service_request.headers()) { // TODO В такой реализации При инъекции в РекуестХэндлер этот объкт будет склонирован. А нужно передать его самого. Актикс сейчас не позволякет это сделать. Проверить позже
+                Ok(json_access_web_token) => {
+                    service_request.extensions_mut().insert(json_access_web_token);
+
+                    return Either::Left(self.service.call(service_request));
+                },
+                Err(ref base_error) => {
+                    match base_error {
+                        BaseError::EntityError {entity_error} => {
+                            match entity_error {
+                                EntityError::JsonAccessWebTokenError {json_access_web_token_error} => {
+                                    match json_access_web_token_error {
+                                        JsonAccessWebTokenError::AlreadyExpired => {
+                                            match rmp_serde::to_vec(&ResponseDataWrapper::wrap_for_fail(
+                                                CommunicationCodeStorage::ENTITY_JSON_ACCESS_WEB_TOKEN_ALREADY_EXPIRED
+                                            )) {
+                                                Ok(data) => {
+                                                    return Either::Right(future::ok(service_request.into_response(ResponseCreator::create_ok(data).into_body())));
+                                                },
+                                                Err(error) => {
+                                                    log::error!("{}", BaseError::from(error));
+                            
+                                                    return Either::Right(future::ok(service_request.into_response(ResponseCreator::create_internal_server_error().into_body())));
+                                                }
+                                            }
                                         },
-                                        Err(error) => {
-                                            log::error!("{}", BaseError::from(error));
-                    
-                                            return Either::Right(future::ok(service_request.into_response(ResponseCreator::create_internal_server_error().into_body())));
+                                        JsonAccessWebTokenError::InJsonAccessWebTokenBlackList |
+                                        JsonAccessWebTokenError::NotFound => {
+                                            return Either::Right(future::ok(service_request.into_response(ResponseCreator::create_unauthorized().into_body())));
+                                        },
+                                        _ => {
+                                            unreachable!("{}", base_error);
                                         }
                                     }
-                                },
-                                JsonAccessWebTokenError::InJsonAccessWebTokenBlackList |
-                                JsonAccessWebTokenError::NotFound => {
-                                    return Either::Right(future::ok(service_request.into_response(ResponseCreator::create_unauthorized().into_body())));
                                 },
                                 _ => {
                                     unreachable!("{}", base_error);
                                 }
                             }
                         },
-                        _ => {
-                            unreachable!("{}", base_error);
+                        BaseError::InvalidArgumentError => {
+                            return Either::Right(future::ok(service_request.into_response(ResponseCreator::create_bad_request().into_body())));
+                        },
+                        BaseError::LogicError {logic_error: _} |
+                        BaseError::RunTimeError {run_time_error: _} => {
+                            log::error!("{}", base_error);
+
+                            return Either::Right(future::ok(service_request.into_response(ResponseCreator::create_internal_server_error().into_body())));
                         }
                     }
-                },
-                BaseError::InvalidArgumentError => {
-                    return Either::Right(future::ok(service_request.into_response(ResponseCreator::create_bad_request().into_body())));
-                },
-                BaseError::LogicError {logic_error: _} |
-                BaseError::RunTimeError {run_time_error: _} => {
-                    log::error!("{}", base_error);
-
-                    return Either::Right(future::ok(service_request.into_response(ResponseCreator::create_internal_server_error().into_body())));
                 }
             }
         }
 
-        return Either::Left(self.service.call(service_request));
+        log::error!("{}", BaseError::LogicError {logic_error: LogicError::new(true, "Aggregate_connection_pool must exist in application state.")});
+
+        return Either::Right(future::ok(service_request.into_response(ResponseCreator::create_internal_server_error().into_body())));
     }
 }
