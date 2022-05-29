283137e920f4328b5d8512d66dcb1982f84104ce
ZiminDmitriy <dimazimin96@yandex.ru>
2022-05-30 00:42:15 +0300

    updated

--- project/application/source/presentation_layer/service/action_round_logger/mod.rs
+++ project/application/source/presentation_layer/service/action_round_logger/mod.rs
@@ -1,14 +1,24 @@
 use crate::infrastructure_layer::error::error_auditor::error_auditor::ErrorAuditor;
 use hyper::Body;
 use hyper::Request;
+use hyper::Response;
+use hyper::body::HttpBody;
 
 pub struct ActionRoundLogger;
 
 impl ActionRoundLogger {
     pub fn log<'a>(
         request: &'a Request<Body>,
-        error_auditor: Option<ErrorAuditor>
+        response: &'a Response<Body>,
+        error_auditor: Option<&'a ErrorAuditor>
     ) -> () {
-        
+        // let message = "REQUEST DTA TODO".to_string();
+
+        // message + response.status().as_u16();
+
+        // if let Some(error_auditor_) = error_auditor {
+            
+        // }
+
     }
 }
