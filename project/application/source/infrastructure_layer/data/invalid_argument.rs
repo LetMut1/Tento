e603c948aba26bdde2affc45b45291d8a20f9f41
ZiminDmitriy <dimazimin96@yandex.ru>
2023-02-16 17:17:19 +0400

    1

--- project/application/source/infrastructure_layer/data/invalid_argument.rs
+++ project/application/source/infrastructure_layer/data/invalid_argument.rs
@@ -1,14 +1,3 @@
-use crate::domain_layer::data::entity_invalid_argument::EntityInvalidArgument;
-
 pub enum InvalidArgument {
-    Entity {
-        entity_invalid_argument: EntityInvalidArgument
-    },
-    Common {
-        common_invalid_argument: CommomInvalidArgument
-    }
-}
-
-pub enum CommomInvalidArgument {
     HttpHeaders
 }
