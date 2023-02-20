3d8f71c3fc0e1e9d1f606917816eee20a177eebf
ZiminDmitriy <dimazimin96@yandex.ru>
2023-02-21 01:07:05 +0400

    1

--- project/application/source/domain_layer/functionality/service/application_user__validator.rs
+++ project/application/source/domain_layer/functionality/service/application_user__validator.rs
@@ -12,9 +12,10 @@ impl ApplicationUser_Validator {
     const APPLICATION_USER__NICKNAME_MAXIMUM_LENGTH: usize = 55;
     const APPLICATION_USER__PASSWORD_MINIMUM_LENGTH: usize = 7;
     const APPLICATION_USER__PASSWORD_MAXIMUM_LENGTH: usize = 65;
+    const APPLICATION_USER__EMAIL_REGULAR_EXPRESSION: &'static str = r#"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#;
 
     pub fn is_valid_email<'a>(application_user_email: &'a str) -> Result<bool, ErrorAuditor> {
-        let regex = match Regex::new(r"\S+@{1}\S+") {
+        let regex = match Regex::new(Self::APPLICATION_USER__EMAIL_REGULAR_EXPRESSION) {
             Ok(regex_) => regex_,
             Err(error) => {
                 return Err(
