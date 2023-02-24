07a8f53ba4fc02d037c01bf94bc39eb80c14f541
ZiminDmitriy <dimazimin96@yandex.ru>
2023-02-24 16:32:53 +0400

    1

---
+++ project/application/source/presentation_layer/functionality/command/create_fixtures/mod.rs
@@ -0,0 +1,13 @@
+extern crate application;
+
+use application::application_layer::functionality::service::command_processor::create_fixtures_processor::CreateFixturesProcessor;
+use application::infrastructure_layer::functionality::service::displayer::Display;
+use application::infrastructure_layer::functionality::service::displayer::Displayer;
+
+fn main() -> () {
+    if let Err(error) = RunServerProcessor::process() {
+        println!("{}", Displayer::display(&error));
+    }
+
+    return ();
+}
