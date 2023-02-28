5736bec2c6ee705c33c0eeffd02d6b1b2d5cc366
ZiminDmitriy <dimazimin96@yandex.ru>
2023-02-28 16:50:06 +0400

    1

--- project/application/source/infrastructure_layer/integration_module/mod.rs
+++ project/application/source/infrastructure_layer/integration_module/mod.rs
@@ -1,4 +1,11 @@
+use std::os::raw::c_long;
+
+// TODO DELETE TODO DELETE TODO DELETE TODO DELETE TODO DELETE
 #[no_mangle]
-pub extern "C" fn hello_devworld() {
-    println!("Hello, /dev/world 2019!");
+pub extern "C" fn test1(x: c_long) -> bool {
+    if x > 1 {
+        return true;
+    }
+
+    return false
 }
