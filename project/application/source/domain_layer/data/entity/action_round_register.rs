028c1f3016a4d4c198d0f8a871838610f88e53cd
ZiminDmitriy <dimazimin96@yandex.ru>
2023-03-30 04:20:32 +0400

    1

--- project/application/source/domain_layer/data/entity/action_round_register.rs
+++ project/application/source/domain_layer/data/entity/action_round_register.rs
@@ -1,7 +1,28 @@
+use std::marker::PhantomData;
+
 pub struct ActionRoundRegister {
     route: String,
+    route_: PhantomData<Route>,
+
     method: String,
+    method_: PhantomData<Method>,
+
     status_code: i16,
+    statuse_code_: PhantomData<StatusCode>,
+
     context: Option<String>,
-    created_at: String
+    context_: PhantomData<Context>,
+
+    created_at: String,
+    created_at_: PhantomData<CreatedAt>,
 }
+
+pub struct Route;
+
+pub struct Method;
+
+pub struct StatusCode;
+
+pub struct Context;
+
+pub struct CreatedAt;
