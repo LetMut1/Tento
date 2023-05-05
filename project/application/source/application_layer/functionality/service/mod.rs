f8c7b6542d05c21fb79224d37d6947baeab96644
ZiminDmitriy <dimazimin96@yandex.ru>
2023-05-06 03:14:19 +0400

    1

--- project/application/source/application_layer/functionality/service/mod.rs
+++ project/application/source/application_layer/functionality/service/mod.rs
@@ -1,5 +1,2 @@
 #[cfg(feature = "facilitate_non_automatic_functional_testing")]
-pub mod action_delegator;
-
-#[cfg(feature = "facilitate_non_automatic_functional_testing")]
 pub mod wrapped_action_processor;
