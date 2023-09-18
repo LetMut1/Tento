49f56654e68e5572c1bc79f52dbda9c32dae4723
ZiminDmitriy <dimazimin96@yandex.ru>
2023-09-19 02:10:20 +0500

    1

--- application/application/source/integration_module/test_build_DELETE/c_bindings.h
+++ application/application/source/integration_module/test_build_DELETE/c_bindings.h
@@ -17,6 +17,20 @@ struct C {
   bool b;
 };
 
+struct StructWithString1 {
+  bool is_exist;
+  char *string;
+};
+
+struct Error {
+  bool is_exist;
+};
+
+struct StructWithString2 {
+  StructWithString1 struct_with_string;
+  Error error;
+};
+
 extern "C" {
 
 int f1(int a);
@@ -44,5 +58,20 @@ bool f11(A *a, B *b);
 C f12(C *a, int b);
 
 void f13(C *a, bool b);
+
+char *string_allocate_f1();
+
+void string_deallocate_f1(char *string);
+
+StructWithString1 *string_allocate_f2();
+
+void string_deallocate_f2(StructWithString1 *struct_with_string);
+
+StructWithString2 *string_allocate_f3();
+
+void string_deallocate_f3(StructWithString2 *struct_with_string);
+
+unsigned char array_slice_f1(unsigned char *pointer_to_first_element_of_registry,
+                             size_t registry_length);
 
 } // extern "C"
