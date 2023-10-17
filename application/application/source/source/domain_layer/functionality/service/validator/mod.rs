f74914f2bcb72f9e83923d26905bfdcb6f1d0e3c
ZiminDmitriy <dimazimin96@yandex.ru>
2023-10-18 02:03:13 +0500

    1

--- application/application/source/source/domain_layer/functionality/service/validator/mod.rs
+++ application/application/source/source/domain_layer/functionality/service/validator/mod.rs
@@ -1,16 +1,16 @@
-pub mod application_user__email___validator;
-pub mod application_user__id___validator;
-pub mod application_user__nickname___validator;
-pub mod application_user__password___validator;
-pub mod application_user_authorization_token__value___validator;
-pub mod application_user_device__id___validator;
-pub mod application_user_registration_token__value___validator;
-pub mod application_user_reset_password_token__value___validator;
-pub mod channel__description___validator;
-pub mod channel__id___validator;
-pub mod channel__linked_name___validator;
-pub mod channel__name___validator;
-pub mod channel__orientation___validator;
+pub mod application_user__email;
+pub mod application_user__id;
+pub mod application_user__nickname;
+pub mod application_user__password;
+pub mod application_user_authorization_token__value;
+pub mod application_user_device__id;
+pub mod application_user_registration_token__value;
+pub mod application_user_reset_password_token__value;
+pub mod channel__description;
+pub mod channel__id;
+pub mod channel__linked_name;
+pub mod channel__name;
+pub mod channel__orientation;
 
 use std::marker::PhantomData;
 
