9818d09ca2fa0e3034a979d2076a0a4a4b8f7c60
ZiminDmitriy <dimazimin96@yandex.ru>
2024-06-01 02:37:53 +0300

    1

--- application/application/source/forced_crate/entity/source/application_user_access_refresh_token.rs
+++ application/application/source/forced_crate/entity/source/application_user_access_refresh_token.rs
@@ -58,7 +58,7 @@ pub struct ApplicationUserAccessRefreshToken1 {
     pub application_user_access_token_id: String,
     pub obfuscation_value: String,
     pub expires_at: i64,
-    pub updated_at: UpdatedAt,
+    pub updated_at: i64,
 }
 
 #[derive(Serialize, Deserialize)]
