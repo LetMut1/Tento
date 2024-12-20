9a579daa74b65de60fec0dda72c44542ddc21446
ZiminDmitriy <dimazimin96@yandex.ru>
2024-12-20 19:32:53 +0300

    1

--- application/application/source/foreign_function_interface/client_server_data_serializer/compile_for_targets.sh
+++ application/application/source/foreign_function_interface/client_server_data_serializer/compile_for_targets.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-result=`cargo valgrind test --manifest-path=./source/Cargo.toml`
-if [ $? != 0 ];
-then
-    echo "An error was occured: \n $result ."
-    exit 1
-fi
+# result=`cargo valgrind test --manifest-path=./source/Cargo.toml`
+# if [ $? != 0 ];
+# then
+#     echo "An error was occured: \n $result ."
+#     exit 1
+# fi
 
 result=`cargo build --manifest-path=./source/Cargo.toml --release --lib --target=aarch64-apple-ios`
 if [ $? != 0 ];
@@ -35,28 +35,28 @@ then
     exit 1
 fi
 
-result=`cp ./../../../source/target/aarch64-apple-ios/release/libclient_server_data_serializer.a ./target_/aarch64-apple-ios/libclient_server_data_serializer.a`
+result=`cp ./../../../target/aarch64-apple-ios/release/libclient_server_data_serializer.a ./target_/aarch64-apple-ios/libclient_server_data_serializer.a`
 if [ $? != 0 ];
 then
     echo "An error was occured: \n $result ."
     exit 1
 fi
 
-result=`cp ./../../../source/target/aarch64-apple-ios-sim/release/libclient_server_data_serializer.a ./target_/aarch64-apple-ios-sim/libclient_server_data_serializer.a`
+result=`cp ./../../../target/aarch64-apple-ios-sim/release/libclient_server_data_serializer.a ./target_/aarch64-apple-ios-sim/libclient_server_data_serializer.a`
 if [ $? != 0 ];
 then
     echo "An error was occured: \n $result ."
     exit 1
 fi
 
-result=`cp ./../../../source/target/aarch64-linux-android/release/libclient_server_data_serializer.a ./target_/aarch64-linux-android/libclient_server_data_serializer.a`
+result=`cp ./../../../target/aarch64-linux-android/release/libclient_server_data_serializer.a ./target_/aarch64-linux-android/libclient_server_data_serializer.a`
 if [ $? != 0 ];
 then
     echo "An error was occured: \n $result ."
     exit 1
 fi
 
-result=`cp ./../../../source/target/x86_64-linux-android/release/libclient_server_data_serializer.a ./target_/x86_64-linux-android/libclient_server_data_serializer.a`
+result=`cp ./../../../target/x86_64-linux-android/release/libclient_server_data_serializer.a ./target_/x86_64-linux-android/libclient_server_data_serializer.a`
 if [ $? != 0 ];
 then
     echo "An error was occured: \n $result ."
