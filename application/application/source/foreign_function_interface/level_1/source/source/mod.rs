9411ae99dcae1e71ff8d2743c77011274dfe353d
ZiminDmitriy <dimazimin96@yandex.ru>
2025-01-09 02:18:10 +0300

    1

--- application/application/source/foreign_function_interface/level_1/source/source/mod.rs
+++ application/application/source/foreign_function_interface/level_1/source/source/mod.rs
@@ -1,5 +1,5 @@
 use libc::c_char;
-
+// level_1---------------------------------------------------------------------------------
 #[no_mangle]
 pub extern "C" fn get_number() -> c_char {
     return 69;
