c6bd5951e7d4f9752e212c8ea54b95dddee646f0
ZiminDmitriy <dimazimin96@yandex.ru>
2025-01-10 23:19:53 +0300

    1

--- application/application/source/foreign_function_interface/client_server_data_serializer/compile_for_targets.sh
+++ application/application/source/foreign_function_interface/client_server_data_serializer/compile_for_targets.sh
@@ -23,6 +23,12 @@ then
     echo "An error was occured: \n $result ."
     exit 1
 fi
+result=`cargo build --manifest-path=./source/Cargo.toml --release --lib --target=x86_64-apple-ios`
+if [ $? != 0 ];
+then
+    echo "An error was occured: \n $result ."
+    exit 1
+fi
 result=`cargo build --manifest-path=./source/Cargo.toml --release --lib --target=x86_64-linux-android`
 if [ $? != 0 ];
 then
@@ -42,6 +48,12 @@ then
     exit 1
 fi
 result=`cp ./../../../target/aarch64-linux-android/release/libclient_server_data_serializer.a ./target_/aarch64-linux-android/libclient_server_data_serializer.a`
+if [ $? != 0 ];
+then
+    echo "An error was occured: \n $result ."
+    exit 1
+fi
+result=`cp ./../../../target/x86_64-apple-ios/release/libclient_server_data_serializer.a ./target_/x86_64-apple-ios/libclient_server_data_serializer.a`
 if [ $? != 0 ];
 then
     echo "An error was occured: \n $result ."
