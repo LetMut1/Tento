c18c11afab2040319c47fecaf6e03259f0711fe7
ZiminDmitriy <dimazimin96@yandex.ru>
2025-02-22 12:52:34 +0300

    1

--- application/core/source/source/domain_layer/data/entity/channel_outer_link/mod.rs
+++ application/core/source/source/domain_layer/data/entity/channel_outer_link/mod.rs
@@ -9,29 +9,17 @@ use {
     std::marker::PhantomData,
 };
 pub struct ChannelOuterLink {
-    pub from: i64,
+    from: i64,
     _from: PhantomData<Channel_Id>,
-    pub alias: String,
+    alias: String,
     _alias: PhantomData<Alias>,
-    pub address: String,
+    address: String,
     _address: PhantomData<Address>,
-    pub created_at: i64,
+    created_at: i64,
     _created_at: PhantomData<CreatedAt>,
 }
 impl ChannelOuterLink {
     pub const MAXIMUM_QUANTITY: i16 = 5;
-    pub fn new(from: i64, alias: String, address: String, created_at: i64) -> Self {
-        return Self {
-            from,
-            _from: PhantomData,
-            alias,
-            _alias: PhantomData,
-            address,
-            _address: PhantomData,
-            created_at,
-            _created_at: PhantomData,
-        };
-    }
 }
 pub type ChannelOuterLink_Address = Address;
 pub type ChannelOuterLink_Alias = Alias;
