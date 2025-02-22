f44d614b5713903dc56f9801a17a9c60164c829e
ZiminDmitriy <dimazimin96@yandex.ru>
2025-02-22 12:50:47 +0300

    1

--- application/core/source/source/domain_layer/data/entity/channel_subscription/mod.rs
+++ application/core/source/source/domain_layer/data/entity/channel_subscription/mod.rs
@@ -8,23 +8,11 @@ use {
     std::marker::PhantomData,
 };
 pub struct ChannelSubscription {
-    pub user__id: i64,
+    user__id: i64,
     _user__id: PhantomData<User_Id>,
-    pub channel__id: i64,
+    channel__id: i64,
     _channel__id: PhantomData<Channel_Id>,
-    pub created_at: i64,
+    created_at: i64,
     _created_at: PhantomData<CreatedAt>,
-}
-impl ChannelSubscription {
-    pub fn new(user__id: i64, channel__id: i64, created_at: i64) -> Self {
-        return Self {
-            user__id,
-            _user__id: PhantomData,
-            channel__id,
-            _channel__id: PhantomData,
-            created_at,
-            _created_at: PhantomData,
-        };
-    }
 }
 pub type ChannelSubscription_CreatedAt = CreatedAt;
