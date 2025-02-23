b3eda32a16b946d6cf55fed8b874e251068a3c44
ZiminDmitriy <dimazimin96@yandex.ru>
2025-02-23 14:51:41 +0300

    1

--- application/core/source/source/domain_layer/data/entity/channel_inner_link/mod.rs
+++ application/core/source/source/domain_layer/data/entity/channel_inner_link/mod.rs
@@ -5,24 +5,14 @@ use {
     std::marker::PhantomData,
 };
 pub struct ChannelInnerLink {
-    pub from: i64,
+    from: i64,
     _from: PhantomData<Channel_Id>,
-    pub to: i64,
+    to: i64,
     _to: PhantomData<Channel_Id>,
-    pub created_at: i64,
+    created_at: i64,
     _created_at: PhantomData<CreatedAt>,
 }
 impl ChannelInnerLink {
     pub const MAXIMUM_QUANTITY: i16 = 10;
-    pub fn new(from: i64, to: i64, created_at: i64) -> Self {
-        return Self {
-            from,
-            _from: PhantomData,
-            to,
-            _to: PhantomData,
-            created_at,
-            _created_at: PhantomData,
-        };
-    }
 }
 pub type ChannelInnerLink_CreatedAt = CreatedAt;
