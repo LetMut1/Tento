3298e401f2f5d64a3b2143bee839a64f98c7ad0e
ZiminDmitriy <dimazimin96@yandex.ru>
2025-03-13 12:13:48 +0300

    _

---
+++ application/core/source/source/infrastructure_layer/functionality/service/encoder/hmac_sha2_256.rs
@@ -0,0 +1,55 @@
+use {
+    super::Encoder,
+    crate::infrastructure_layer::data::aggregate_error::AggregateError,
+    hmac::{
+        Hmac,
+        Mac,
+    },
+    sha2::Sha256,
+};
+pub type HmacSha2_256 = Hmac<Sha256>;
+impl Encoder<HmacSha2_256> {
+    pub fn encode<'a>(salt: &'a [u8], data_for_encode: &'a [u8]) -> Result<Vec<u8>, AggregateError> {
+        return Result::Ok(
+            Self::prepare(
+                salt,
+                data_for_encode,
+            )?
+            .finalize()
+            .into_bytes()
+            .to_vec(),
+        );
+    }
+    pub fn is_valid<'a>(salt: &'a [u8], data_for_encode: &'a [u8], encoded_data: &'a [u8]) -> Result<bool, AggregateError> {
+        return Result::Ok(
+            Self::prepare(
+                salt,
+                data_for_encode,
+            )?
+            .verify_slice(encoded_data)
+            .is_ok(),
+        );
+    }
+    fn prepare<'a>(salt: &'a [u8], data_for_encode: &'a [u8]) -> Result<HmacSha2_256, AggregateError> {
+        let mut hmac = crate::result_return_logic!(HmacSha2_256::new_from_slice(salt));
+        hmac.update(data_for_encode);
+        return Result::Ok(hmac);
+    }
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn validate() -> Result<(), Box<dyn std::error::Error + 'static>> {
+        let salt = [0 as u8, 1, 2, 3];
+        let data_for_encode = [0 as u8, 1];
+        if !Encoder::<HmacSha2_256>::is_valid(
+            salt.as_slice(),
+            data_for_encode.as_slice(),
+            Encoder::<HmacSha2_256>::encode(salt.as_slice(), data_for_encode.as_slice())?.as_slice(),
+        )? {
+            return Result::Err("".into());
+        }
+        return Result::Ok(());
+    }
+}
--- application/core/source/source/infrastructure_layer/functionality/service/encoder/mod.rs
+++ application/core/source/source/infrastructure_layer/functionality/service/encoder/mod.rs
@@ -1,9 +1,10 @@
 mod argon2id;
-mod hmac;
+mod hmac_sha3_512;
+mod hmac_sha2_256;
 mod highway;
 pub use self::{
     argon2id::Argon2Id,
-    hmac::HmacSha3_512,
+    hmac_sha3_512::HmacSha3_512,
     highway::Highway,
 };
 use std::marker::PhantomData;
