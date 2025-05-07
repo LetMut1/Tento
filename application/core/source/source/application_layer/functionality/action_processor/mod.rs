b1af92ec0c34d8dc7dc909986fe2c414b927f500
ZiminDmitriy <dimazimin96@yandex.ru>
2025-05-07 11:25:07 +0300

    1

--- application/core/source/source/application_layer/functionality/action_processor/mod.rs
+++ application/core/source/source/application_layer/functionality/action_processor/mod.rs
@@ -5,56 +5,6 @@ mod channel_publication1_mark;
 mod channel_publication1_view;
 mod channel_subscription;
 mod user;
-pub use self::{
-    channel::{
-        check_linked_name_for_existing::Channel_CheckLinkedNameForExisting,
-        check_name_for_existing::Channel_CheckNameForExisting,
-        create::Channel_Create,
-        get_many_by_name_in_subscriptions::Channel_GetManyByNameInSubscriptions,
-        get_many_by_subscription::Channel_GetManyBySubscription,
-        get_many_public_by_name::Channel_GetManyPublicByName,
-        get_one_by_id::Channel_GetOneById,
-        delete::Channel_Delete,
-        get_many_owned::Channel_GetManyOwned,
-        refresh_channel_token::Channel_RefreshChannelToken,
-    },
-    channel_publication1::{
-        create::ChannelPublication1_Create,
-        delete::ChannelPublication1_Delete,
-        get_many::ChannelPublication1_GetMany,
-    },
-    channel_publication1_commentary::{
-        create::ChannelPublication1Commentary_Create,
-        delete::ChannelPublication1Commentary_Delete,
-    },
-    channel_publication1_mark::{
-        create::ChannelPublication1Mark_Create,
-        delete::ChannelPublication1Mark_Delete,
-    },
-    channel_publication1_view::create::ChannelPublication1View_Create,
-    channel_subscription::{
-        create::ChannelSubscription_Create,
-        delete::ChannelSubscription_Delete,
-    },
-    user::{
-        authorize_by_first_step::User_AuthorizeByFirstStep,
-        authorize_by_last_step::User_AuthorizeByLastStep,
-        check_email_for_existing::User_CheckEmailForExisting,
-        check_nickname_for_existing::User_CheckNicknameForExisting,
-        deauthorize_from_all_devices::User_DeauthorizeFromAllDevices,
-        deauthorize_from_one_device::User_DeauthorizeFromOneDevice,
-        refresh_access_token::User_RefreshAccessToken,
-        register_by_first_step::User_RegisterByFirstStep,
-        register_by_last_step::User_RegisterByLastStep,
-        register_by_second_step::User_RegisterBySecondStep,
-        reset_password_by_first_step::User_ResetPasswordByFirstStep,
-        reset_password_by_last_step::User_ResetPasswordByLastStep,
-        reset_password_by_second_step::User_ResetPasswordBySecondStep,
-        send_email_for_authorize::User_SendEmailForAuthorize,
-        send_email_for_register::User_SendEmailForRegister,
-        send_email_for_reset_password::User_SendEmailForResetPassword,
-    },
-};
 use {
     super::command_processor::RunServer,
     crate::infrastructure_layer::{
@@ -86,3 +36,39 @@ pub struct Inner<'a> {
     pub postgresql_connection_pool_database_3: &'a PostgresqlConnectionPool,
     pub postgresql_connection_pool_database_4: &'a PostgresqlConnectionPool,
 }
+pub type Channel_CheckLinkedNameForExisting = self::channel::check_linked_name_for_existing::Channel_CheckLinkedNameForExisting;
+pub type Channel_CheckNameForExisting = self::channel::check_name_for_existing::Channel_CheckNameForExisting;
+pub type Channel_Create = self::channel::create::Channel_Create;
+pub type Channel_GetManyByNameInSubscriptions = self::channel::get_many_by_name_in_subscriptions::Channel_GetManyByNameInSubscriptions;
+pub type Channel_GetManyBySubscription = self::channel::get_many_by_subscription::Channel_GetManyBySubscription;
+pub type Channel_GetManyPublicByName = self::channel::get_many_public_by_name::Channel_GetManyPublicByName;
+pub type Channel_GetOneById = self::channel::get_one_by_id::Channel_GetOneById;
+pub type Channel_Delete = self::channel::delete::Channel_Delete;
+pub type Channel_GetManyOwned = self::channel::get_many_owned::Channel_GetManyOwned;
+pub type Channel_RefreshChannelToken = self::channel::refresh_channel_token::Channel_RefreshChannelToken;
+pub type ChannelPublication1_Create = self::channel_publication1::create::ChannelPublication1_Create;
+pub type ChannelPublication1_Delete = self::channel_publication1::delete::ChannelPublication1_Delete;
+pub type ChannelPublication1_GetMany = self::channel_publication1::get_many::ChannelPublication1_GetMany;
+pub type ChannelPublication1Commentary_Create = self::channel_publication1_commentary::create::ChannelPublication1Commentary_Create;
+pub type ChannelPublication1Commentary_Delete = self::channel_publication1_commentary::delete::ChannelPublication1Commentary_Delete;
+pub type ChannelPublication1Mark_Create = self::channel_publication1_mark::create::ChannelPublication1Mark_Create;
+pub type ChannelPublication1Mark_Delete = self::channel_publication1_mark::delete::ChannelPublication1Mark_Delete;
+pub type ChannelPublication1View_Create = self::channel_publication1_view::create::ChannelPublication1View_Create;
+pub type ChannelSubscription_Create = self::channel_subscription::create::ChannelSubscription_Create;
+pub type ChannelSubscription_Delete = self::channel_subscription::delete::ChannelSubscription_Delete;
+pub type User_AuthorizeByFirstStep = self::user::authorize_by_first_step::User_AuthorizeByFirstStep;
+pub type User_AuthorizeByLastStep = self::user::authorize_by_last_step::User_AuthorizeByLastStep;
+pub type User_CheckEmailForExisting = self::user::check_email_for_existing::User_CheckEmailForExisting;
+pub type User_CheckNicknameForExisting = self::user::check_nickname_for_existing::User_CheckNicknameForExisting;
+pub type User_DeauthorizeFromAllDevices = self::user::deauthorize_from_all_devices::User_DeauthorizeFromAllDevices;
+pub type User_DeauthorizeFromOneDevice = self::user::deauthorize_from_one_device::User_DeauthorizeFromOneDevice;
+pub type User_RefreshAccessToken = self::user::refresh_access_token::User_RefreshAccessToken;
+pub type User_RegisterByFirstStep = self::user::register_by_first_step::User_RegisterByFirstStep;
+pub type User_RegisterByLastStep = self::user::register_by_last_step::User_RegisterByLastStep;
+pub type User_RegisterBySecondStep = self::user::register_by_second_step::User_RegisterBySecondStep;
+pub type User_ResetPasswordByFirstStep = self::user::reset_password_by_first_step::User_ResetPasswordByFirstStep;
+pub type User_ResetPasswordByLastStep = self::user::reset_password_by_last_step::User_ResetPasswordByLastStep;
+pub type User_ResetPasswordBySecondStep = self::user::reset_password_by_second_step::User_ResetPasswordBySecondStep;
+pub type User_SendEmailForAuthorize = self::user::send_email_for_authorize::User_SendEmailForAuthorize;
+pub type User_SendEmailForRegister = self::user::send_email_for_register::User_SendEmailForRegister;
+pub type User_SendEmailForResetPassword = self::user::send_email_for_reset_password::User_SendEmailForResetPassword;
