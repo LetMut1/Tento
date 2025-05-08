480fb4ac2b5da818a3465386ecc3dd8c00cdab74
ZiminDmitriy <dimazimin96@yandex.ru>
2025-05-08 18:21:37 +0300

    1

--- application/core/source/source/infrastructure_layer/data/mod.rs
+++ application/core/source/source/infrastructure_layer/data/mod.rs
@@ -2,3 +2,4 @@ pub mod aggregate_error;
 pub mod control_type;
 pub mod environment_configuration;
 pub mod server_workflow_error;
+pub mod sended;
---
+++ application/core/source/source/infrastructure_layer/data/sended.rs
@@ -0,0 +1,24 @@
+use std::mem::ManuallyDrop;
+#[derive(Clone)]
+pub struct Sended<T>(*mut T);
+impl<T> Sended<T> {
+    pub fn new(pointer: *mut T) -> Self {
+        return Self(pointer);
+    }
+    pub unsafe fn write(self, value: T) -> () {
+        unsafe { self.0.write(value); }
+        return ();
+    }
+    pub unsafe fn read(self) -> ManuallyDrop<T> {
+        return unsafe { ManuallyDrop::new(self.0.read()) };
+    }
+    pub unsafe fn read_<'a>(self) -> &'a T {
+        return unsafe { &*self.0 };
+    }
+}
+unsafe impl<T> Send for Sended<T> {}
+unsafe impl<T> Sync for Sended<T> {}
+impl<T> Copy for Sended<T>
+where
+    Self: Clone
+{}
